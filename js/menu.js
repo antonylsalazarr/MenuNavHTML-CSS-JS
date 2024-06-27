11cd25a5a5ae27ef57ee0ea2624b25dabee3f8c6
antonylsalazarr <antony.salazar.2202@gmail.com>
2024-06-27 08:27:19 -0500

    Se agrega el proyecto

---
+++ js/menu.js
@@ -0,0 +1,43 @@
+//1
+function mostrar1() {
+    const ocultItem = document.getElementById('divseccion1');
+    ocultItem.style.display = 'block';
+};
+
+function ocultar1() {
+    const ocultItem = document.getElementById('divseccion1');
+    ocultItem.style.display = 'none';
+};
+
+//2
+function mostrar2() {
+    const ocultItem = document.getElementById('divseccion2');
+    ocultItem.style.display = 'block';
+};
+
+function ocultar2() {
+    const ocultItem = document.getElementById('divseccion2');
+    ocultItem.style.display = 'none';
+};
+
+//3
+function mostrar3() {
+    const ocultItem = document.getElementById('divseccion3');
+    ocultItem.style.display = 'block';
+};
+
+function ocultar3() {
+    const ocultItem = document.getElementById('divseccion3');
+    ocultItem.style.display = 'none';
+};
+
+//4
+function mostrar4() {
+    const ocultItem = document.getElementById('divseccion4');
+    ocultItem.style.display = 'block';
+};
+
+function ocultar4() {
+    const ocultItem = document.getElementById('divseccion4');
+    ocultItem.style.display = 'none';
+};
